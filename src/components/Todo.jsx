472e773c4a8ffca39a3d1fb7e1fda7e5fe0bf7ea
kubat <kubat.aiupov@gmail.com>
2023-07-11 22:27:53 +0200

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { ACTIONS } from "../App";
+
+export default function Todo({ todo, dispatch }) {
+  return (
+    <div>
+      <span style={{ color: todo.complete ? "#AAA" : "#000" }}>
+        {todo.name}
+      </span>
+      <button
+        onClick={() =>
+          dispatch({
+            type: ACTIONS.TOGGLE_TODO,
+            payload: {
+              id: todo.id,
+            },
+          })
+        }
+      >
+        toggle
+      </button>
+      <button onClick={() =>
+        dispatch({
+          type: ACTIONS.DELETE_TODO,
+          payload: {
+            id: todo.id,
+          },
+        })
+      }>delete</button>
+    </div>
+  );
+}
